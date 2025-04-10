ce22b2dca7bcb738df4a1237ce4390d7e069c3da
hyunseo <ols3040015@naver.com>
2025-04-10 21:56:31 +0900

    feat: upload week04 mission2 and mission3

---
+++ week04/week04_mission2,3/src/App.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import LoginPage from "./pages/LoginPage";
+import HomeLayout from "./layouts/HomeLayout";
+import SignupPage from "./pages/SignupPage";
+import MyPage from "./pages/MyPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "signup", element: <SignupPage /> },
+      { path: "mypage", element: <MyPage /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ week04/week04_mission2,3/src/apis/axios.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosInstance } from "axios";
+import { LOCAL_STORAGE_KEY } from "../constants/key";
+
+export const axiosInstance: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_SERVER_API_URL,
+  // headers: {
+  //   Authorization: `Bearer ${localStorage.getItem(
+  //     LOCAL_STORAGE_KEY.accessToken
+  //   )}`,
+  // },
+});
+
+// 요청 인터셉터 설정
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem(LOCAL_STORAGE_KEY.accessToken);
+
+  // 회원가입, 로그인 요청일 땐 Authorization 헤더 생략
+  const isAuthEndpoint =
+    config.url?.includes("/auth/signup") ||
+    config.url?.includes("/auth/signin");
+
+  if (token && !isAuthEndpoint) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
