b5082f150a622d4a6c948382f909b34d65ec9c53
hyunseo <ols3040015@naver.com>
2025-05-16 14:27:56 +0900

    refactor: code refactoring

--- week07/src/hooks/mutations/useDeleteLike.ts
+++ week07/src/hooks/mutations/useDeleteLike.ts
@@ -8,23 +8,17 @@ import { ResponseMyInfoDto } from "../../types/auth";
 function useDeleteLike() {
   return useMutation({
     mutationFn: deleteLike,
-    // onMutate -> API 요청 이전에 호출되는 친구
     onMutate: async (lp) => {
-      // 1. 이 게시글의 관련된 쿼리를 취소 (캐시된 데이터를 새로 불러오는 요청)
       await queryClient.cancelQueries({
         queryKey: [QUERY_KEY.lps, lp.lpId],
       });
-      // 2. 현재 게시글의 데이터를 캐시에서 가져와야 함
       const previousLpPost = queryClient.getQueryData<ResponseLpDto>([
         QUERY_KEY.lps,
         lp.lpId,
       ]);
 
-      // 3. 게시글 데이터를 복사해서 NewLpPost라는 새로운 객체를 만듦
-      // 복사하는 가장 큰 이유는 나중에 오류가 발생했을 때 이전 상태로 되돌리기 위해서라고 생각하면 됨
       const newLpPost = { ...previousLpPost };
 
-      // 4. 게시글에 저장된 좋아요 목록에서 현재 좋아요의 위치를 찾아야 함
       const me = queryClient.getQueryData<ResponseMyInfoDto>([
         QUERY_KEY.myInfo,
       ]);
@@ -42,8 +36,6 @@ function useDeleteLike() {
         previousLpPost?.data.likes.push(newLike);
       }
 
-      // 업데이트된 게시글 데이터를 캐시에 저장
-      // 이렇게 하면 UI가 바로 업데이트됨, 사용자가 변화를 확인할 수 있다.
       queryClient.setQueryData([QUERY_KEY.lps, lp.lpId], newLpPost);
 
       return { previousLpPost, newLpPost };
@@ -57,7 +49,6 @@ function useDeleteLike() {
       );
     },
 
-    // onSettled는 API 요청이 끝난 후 (성공하든 실패하든 실행)
     onSettled: async (data, error, variables) => {
       await queryClient.invalidateQueries({
         queryKey: [QUERY_KEY.lps, variables.lpId],
